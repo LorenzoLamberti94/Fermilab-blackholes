f98cf81aa79ff8dd46d43dee968c6c7e958cc4d6
Kevin Retzke <kretzke@fnal.gov>
2018-09-25 12:31:17 -0500

    publish blackhole events to kafka and track in table

--- blackhole_detector.py
+++ blackhole_detector.py
@@ -25,7 +25,7 @@ opts = {
     },
     'window_m': 30,
     'scale': 0.5,
-    'threshold': 100,
+    'threshold': 1,
 }
 
 es = Elasticsearch(opts['elasticsearch_url'], timeout=opts['elasticsearch_timeout_s'])
@@ -35,36 +35,47 @@ bh = Blackholes(client=es, weights=opts['weights'], window=opts['window_m'], sca
 class BlackholeEvent(faust.Record):
     type: str
     name: str
+    detected_at: str
     score: float
     reason: str
 
 
 app = faust.App('blackholes', broker='kafka://lssrv03,lssrv04,lssrv05')
-channel = app.channel(value_type=BlackholeEvent)
-#self.topic = app.topic('detected_blackholes'), value_type=BlackholeEvent)
+detected_blackholes = app.topic('blackholes_detected', value_type=BlackholeEvent)
+known_blackholes = app.Table('known_blackholes', value_type=BlackholeEvent)
 
 
-@app.agent(channel)
+@app.agent(detected_blackholes)
 async def process(stream):
     async for event in stream:
-        print(f'Received: {event!r}')
+        known_blackholes[event.name]=event
+        #print(f'Received: {event!r}')
 
 
-#@app.timer(60)
-@app.task
+@app.timer(300)
 async def detect():
-    holes = bh.get_blackholes(datetime.datetime.utcnow())
+    when = datetime.datetime.utcnow()
+    holes = bh.get_blackholes(when)
     for node,stats in holes.items():
         if stats['score']>opts['threshold']:
-            m = BlackholeEvent(type='node',
-                               name=node,
-                               score=stats['score'],
-                               reason=json.dumps(stats['criteria']),
-            )
-            await channel.send(value=m)
+            if node in known_blackholes:
+                logger.info(f'existing blackhole node={node} score={stats["score"]:.1f}')
+            else:
+                logger.info(f'blackhole node={node} score={stats["score"]:.1f} criteria={stats["criteria"]!r}')
+                m = BlackholeEvent(type='node',
+                                   name=node,
+                                   detected_at=when.isoformat(),
+                                   score=stats['score'],
+                                   reason=json.dumps(stats['criteria']),
+                )
+                await detected_blackholes.send(value=m)
         elif stats['score']>1:
-            logger.info(f'near blackhole node={node} score={stats["score"]} criteria={stats["criteria"]!r}')
+            logger.info(f'near-blackhole node={node} score={stats["score"]:.1f} criteria={stats["criteria"]!r}')
 
+
+@app.page('/list/')
+async def list_blackholes(web, request):
+    return web.text(known_blackholes.as_ansitable())
 
 if __name__=="__main__":
     app.main()
